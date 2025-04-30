3f907b9b357a35ee2c573305f7176a45934ebd33
Vinjar <vinjarkvamme@gmail.com>
2025-04-30 22:30:16 +0200

    Init commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Container, Subtitle, Title } from './components/styled';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Title>Velkommen til BarselBuddy 👋</Title>
+      <Subtitle>Her finner du oversikt over alt i barseltiden.</Subtitle>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+	padding: 16px;
+	background-color: #f5f5f5;
+`;
+
+export const Title = styled.Text`
+	font-size: 24px;
+	font-weight: bold;
+	margin-bottom: 10px;
+`;
+
+export const Subtitle = styled.Text`
+	font-size: 16px;
+	color: #555;
+`;
